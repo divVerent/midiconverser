3784222f2c0d59182a645e3e81cb89206ce63ed2
Rudolf Polzer <divVerent@gmail.com>
2024-07-06 20:12:51 -0400

    Initial import.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/divVerent/midiconverser
+
+go 1.22.5
+
+require gitlab.com/gomidi/midi/v2 v2.1.7
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	_ "gitlab.com/gomidi/midi/v2/sequencer"
+	_ "gitlab.com/gomidi/midi/v2/smf"
+)
+
+func main() {
+}
