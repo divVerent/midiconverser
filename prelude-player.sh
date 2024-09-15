d5fe5aa01a1e4e7cecc5305273bd94bd26e74376
Rudolf Polzer <divVerent@gmail.com>
2024-09-15 07:13:00 -0400

    Also add a player for prelude.

---
+++ prelude-player.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+player=$1
+repeat=${2:-2}
+sleep=${3:-2}
+
+prefix=
+
+panic() {
+	echo "Stopping all notes..."
+	if [ -n "$prefix" ]; then
+		$player "$prefix.panic.mid"
+	fi
+	exit 1
+}
+
+trap panic EXIT
+
+trap 'exit 1' INT
+
+LF='
+'
+
+while :; do
+	verses=$(echo *.verse.mid | xargs -n 1 | shuf)"$LF"
+	while [ -n "$verses" ]; do
+		verse=${verses%%$LF*}
+		verses=${verses#*$LF}
+		prefix=${verse%.verse.mid}
+		thisrepeat=$repeat
+		# HACK; better migrate to a JSON file.
+		if grep -q -- -verses=1 "$prefix.sh"; then
+			# If -verses=1, then repeats are baked in.
+			# These are too long - skip them.
+			echo "Skipping $prefix due to baked-in repeats."
+			continue
+		fi
+		for i in $(seq 1 $thisrepeat); do
+			echo "Playing repeat $i/$thisrepeat of verse of $prefix..."
+			$player "$prefix.verse.mid"
+			echo "Waiting $sleep seconds..."
+			sleep 2
+		done
+	done
+done
