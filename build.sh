e47b625688c8b1316cf07f25e508785ca702fdec
Rudolf Polzer <divVerent@gmail.com>
2024-10-05 08:38:12 -0400

    Minor build fixes.

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ go_build_release() {
 
 # Special flags for even smaller binaries.
 go_build_embedrelease() {
-	go build -a -ldflags=all="-s -w $EXTRA_LDFLAGS" -gcflags=all='-B -dwarf=false -l' -trimpath "$@"
+	go build -a -ldflags=all="-s -w $EXTRA_LDFLAGS" -gcflags=all='-B -dwarf=false -l' -trimpath -tags embed "$@"
 }
 
 win32() {
@@ -38,7 +38,7 @@ wasm() {
 	)
 }
 
-win32 go_build_release -o ebitenui_player_nodata.exe ./cmd/ebitenui_player
+win32 go_build_release -o ebitenui_player.exe ./cmd/ebitenui_player
 
 if [ -d ../midi ]; then
 	rm -rf cmd/ebitenui_player/vfs
