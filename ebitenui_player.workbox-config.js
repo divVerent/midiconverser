b41da2d4edc7719687609e11eddd0387a46c97ec
Rudolf Polzer <divVerent@gmail.com>
2024-10-14 10:01:13 -0400

    Try forcing same-origin credentials.

--- ebitenui_player.workbox-config.js
+++ ebitenui_player.workbox-config.js
@@ -8,6 +8,15 @@ module.exports = {
     'wasm_exec.js'
   ],
   swDest: 'ebitenui_player.service-worker.js',
+  runtimeCaching: [{
+    handler: 'StaleWhileRevalidate',
+    options: {
+      fetchOptions: {
+        credentials: 'same-origin'
+      }
+    },
+    urlPattern: /.*/
+  }],
   maximumFileSizeToCacheInBytes: 1073741824,
   inlineWorkboxRuntime: true,
   sourcemap: false
