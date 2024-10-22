5e5705e29420e350c4f0f31e431da792f107ddef
Rudolf Polzer <divVerent@gmail.com>
2024-10-22 16:48:07 -0700

    Fix vkbd more.

--- internal/ebiplayer/vkbd_ios.go
+++ internal/ebiplayer/vkbd_ios.go
@@ -70,12 +70,15 @@ var vKeys = func() [][]vKey {
 		return k
 	}
 	// This is derived from the iPad keyboard on iOS 12.5.7.
+	// Removed: anything non ASCII.
+	// Added: missing backtick.
+	// Now covers all ASCII printables.
 	return [][]vKey{
 		{
 			n("qQ11"), n("wW22"), n("eE33"), n("rR44"), n("tT55"), n("yY66"), n("uU77"), n("iI88"), n("oO99"), n("pP00"),
 		},
 		{
-			n("aA@@"), n("sS##"), n("dD$$"), n("fF&_"), n("gG*^"), n("hH(["), n("jJ)]"), n("kK'{"), n("lL\"}"), backspace,
+			n("aA@@"), n("sS##"), n("dD$`"), n("fF&_"), n("gG*^"), n("hH(["), n("jJ)]"), n("kK'{"), n("lL\"}"), backspace,
 		},
 		{
 			shift, n("zZ%%"), n("xX-|"), n("cC+~"), n("vV=="), n("bB/\\"), n("nN;<"), n("mM:>"), n(",!"), n(".?"),
